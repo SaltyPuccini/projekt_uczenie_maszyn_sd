8c9ae7400614760ea8c3f068344843455350ff5d
Kajetan Wojciechowski <kajetan.wojciechowski@estigiti.com>
2024-06-01 22:13:00 +0200

    Refactor loop so it's efficent

--- experiment_utils.py
+++ experiment_utils.py
@@ -1,11 +1,12 @@
 import pathlib
+
 SAVE_LOCATION_PREFIX = pathlib.Path("/media/kwoj/borrowed/Projekt_Uczenie_Maszyn")
 
-WARM_UP_ITERATIONS = 2
+GUIDANCE_SCALE = 0.0
+WARM_UP_ITERATIONS = 10
+WARM_UP_STEPS = 5
 EXPERIMENT_ITERATIONS = 10
-PROMPT_PREFIX = ("Generate a top-down view of a location in a Japanese Role-Playing Game (J-RPG). The scene should be "
-                 "in a pixel art style and follow a tile-based design. The view must be directly from above. The "
-                 "location is a")
+
 models_dict = {
     "sdxl-turbo": "stabilityai/sdxl-turbo",
     "sd-2-1": "stabilityai/stable-diffusion-2-1",
@@ -17,6 +18,13 @@ prompts_dict = {
     "coastal-town": "deserted island with a treasure chest and wrecked ship.",
     "medieval-city": "medieval city.",
     "path-to-castle": "path through a dark forest leading to a castle gates.",
+    "space-station": "space station on the moon.",
 }
+
+PROMPT_PREFIX = ("Generate a top-down view of a location in a Japanese Role-Playing Game (J-RPG). The scene should be "
+                 "in a pixel art style and follow a tile-based design. The view must be directly from above. The "
+                 "location is a ")
+
+WARM_UP_PROMPT = PROMPT_PREFIX + prompts_dict["forest-village"]
+
 inference_steps = [1, 5, 10, 20, 50, 100]
-guidance_scales = [0.0, 3.0, 7.0, 10.0, 13.0, 17.0, 20.0]
--- expermiental_loop.py
+++ expermiental_loop.py
@@ -9,77 +9,77 @@ from GPU_utils import get_gpu_metrics, initialize_correct_pynvml_device, clear_g
 import gc
 import time
 from experiment_utils import WARM_UP_ITERATIONS, EXPERIMENT_ITERATIONS, models_dict, prompts_dict, PROMPT_PREFIX, \
-    SAVE_LOCATION_PREFIX, inference_steps, guidance_scales
+    SAVE_LOCATION_PREFIX, inference_steps, WARM_UP_STEPS, GUIDANCE_SCALE, WARM_UP_PROMPT
 from logger import logger
 
 # Initialize nvml gpu measurement
 handle = initialize_correct_pynvml_device()
 
 # For each prompt and model, run the benchmarking loop
-for prompt_info in prompts_dict.items():
-    for model_info in models_dict.items():
+for model_info in models_dict.items():
+    logger.info("Running benchmark for model %s ", model_info[0])
+    # Initialize text2img pipeline for selected model
+    pipe = AutoPipelineForText2Image.from_pretrained(model_info[1], torch_dtype=torch.float16,
+                                                     variant="fp16")
+    if model_info[0] == "sdxl-turbo":
+        pipe.enable_model_cpu_offload()
+    else:
+        pipe.to('cuda')
+
+    # Warm up the model to stabilize performance
+    logger.info("Warming up model")
+    for _ in range(WARM_UP_ITERATIONS):
+        gc.collect()
+        torch.cuda.empty_cache()
+        _ = pipe(prompt=(PROMPT_PREFIX + WARM_UP_PROMPT), num_inference_steps=WARM_UP_STEPS,
+                 guidance_scale=GUIDANCE_SCALE).images[0]
+
+    logger.info("Warm up complete")
+
+    for prompt_info in prompts_dict.items():
+        logger.info("Running benchmark for model %s prompt %s", model_info[0], prompt_info[0])
         for inference_step in inference_steps:
-            for guidance_scale in guidance_scales:
-                logger.info("Running benchmark for model %s and prompt %s", model_info[0], prompt_info[0])
+            logger.info("Inferece step %d", inference_step)
 
-                # Initialize text2img pipeline for selected model
-                pipe = AutoPipelineForText2Image.from_pretrained(model_info[1], torch_dtype=torch.float16,
-                                                                 variant="fp16")
-                if model_info[0] == "sdxl-turbo":
-                    pipe.enable_model_cpu_offload()
-                else:
-                    pipe.to('cuda')
-                prompt = (PROMPT_PREFIX + prompt_info[1])
+            # Initialize metrics list and start time
+            metrics = []
+            start_time = time.time()
 
-                # Warm up the model to stabilize performance
-                logger.info("Warming up model")
-                for _ in range(WARM_UP_ITERATIONS):
-                    gc.collect()
-                    torch.cuda.empty_cache()
-                    _ = pipe(prompt=prompt, num_inference_steps=inference_step, guidance_scale=guidance_scale).images[0]
+            # Run the benchmarking loop
+            for i in range(EXPERIMENT_ITERATIONS):
+                logger.info("Running iteration %d", i)
+                # Clear GPU cache
+                clear_gpu_cache()
 
-                logger.info("Warm up complete")
+                # Run inference
+                start_infer_time = time.time()
+                image = \
+                    pipe(prompt=PROMPT_PREFIX + prompt_info[1], num_inference_steps=inference_step,
+                         guidance_scale=GUIDANCE_SCALE).images[0]
 
-                # Initialize metrics list and start time
-                metrics = []
-                start_time = time.time()
-                guidance_str = str(guidance_scale).split(".")[0]
+                # Measure time and GPU metrics
+                end_infer_time = time.time()
+                iteration_metrics = get_gpu_metrics(handle)
+                cpu_metrics = get_cpu_metrics()
+                iteration_metrics.update(cpu_metrics)
+                iteration_metrics['inference_time'] = end_infer_time - start_infer_time
+                metrics.append(iteration_metrics)
 
-                # Run the benchmarking loop
-                logger.info("Running benchmarking loop")
-                for i in range(EXPERIMENT_ITERATIONS):
-                    logger.info("Running iteration %d", i)
-                    # Clear GPU cache
-                    clear_gpu_cache()
+                # Save image
+                image_dir = Path(SAVE_LOCATION_PREFIX,
+                                 f'resulting_images/{model_info[0]}/{prompt_info[0]}/step_{inference_step}')
+                image_dir.mkdir(parents=True, exist_ok=True)
+                image_path = Path(image_dir,
+                                  f'{model_info[0]}_{prompt_info[0]}_step-{inference_step}_iter-{i + 1}.png')
+                image.save(image_path)
 
-                    # Run inference
-                    start_infer_time = time.time()
-                    image = \
-                        pipe(prompt=prompt, num_inference_steps=inference_step, guidance_scale=guidance_scale).images[0]
+            end_time = time.time()
+            logger.info("Benchmarking loop complete")
+            print(f"Total benchmarking time: {end_time - start_time:.2f} seconds")
 
-                    # Measure time and GPU metrics
-                    end_infer_time = time.time()
-                    iteration_metrics = get_gpu_metrics(handle)
-                    cpu_metrics = get_cpu_metrics()
-                    iteration_metrics.update(cpu_metrics)
-                    iteration_metrics['inference_time'] = end_infer_time - start_infer_time
-                    metrics.append(iteration_metrics)
-
-                    # Save image
-                    image_dir = Path(SAVE_LOCATION_PREFIX,
-                                     f'resulting_images/{model_info[0]}/{prompt_info[0]}/step_{inference_step}/guidance_{guidance_str}')
-                    image_dir.mkdir(parents=True, exist_ok=True)
-                    image_path = Path(image_dir,
-                                      f'{model_info[0]}_{prompt_info[0]}_step-{inference_step}_guidance-{guidance_str}_iter-{i + 1}.png')
-                    image.save(image_path)
-
-                end_time = time.time()
-                logger.info("Benchmarking loop complete")
-                print(f"Total benchmarking time: {end_time - start_time:.2f} seconds")
-
-                # Convert metrics to DataFrame for analysis
-                df_metrics = pd.DataFrame(metrics)
-                csv_path = Path(SAVE_LOCATION_PREFIX,
-                                f'{model_info[0]}_{prompt_info[0]}_step-{inference_step}_guidance-{guidance_str}.csv')
-                df_metrics.to_csv(csv_path, index=False)
-                print(df_metrics.describe())
+            # Convert metrics to DataFrame for analysis
+            df_metrics = pd.DataFrame(metrics)
+            csv_path = Path(SAVE_LOCATION_PREFIX, 'partial_csvs'
+                            , f'{model_info[0]}_{prompt_info[0]}_step-{inference_step}.csv')
+            df_metrics.to_csv(csv_path, index=False)
+            print(df_metrics.describe())
