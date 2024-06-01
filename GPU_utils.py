356907994d01e67c55e6f769042eac38537ded13
Kajetan Wojciechowski <kajetan.wojciechowski@estigiti.com>
2024-06-01 20:29:43 +0200

    Add GPU measurement tools

---
+++ GPU_utils.py
@@ -0,0 +1,44 @@
+import os
+import pynvml
+import time
+import gc
+
+import torch
+
+
+def get_gpu_metrics(handle) -> dict:
+    mem_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+    utilization = pynvml.nvmlDeviceGetUtilizationRates(handle)
+    return {
+        'timestamp': time.time(),
+        'gpu_memory_total': mem_info.total,
+        'gpu_memory_used': mem_info.used,
+        'gpu_utilization': utilization.gpu,
+        'memory_utilization': utilization.memory
+    }
+
+
+def clear_gpu_cache() -> None:
+    gc.collect()
+    torch.cuda.empty_cache()
+
+
+def get_nvml_gpu_id(torch_gpu_id: int) -> int:
+    """
+    Remap torch device id to nvml device id, respecting CUDA_VISIBLE_DEVICES.
+
+    If the latter isn't set return the same id
+    """
+    # if CUDA_VISIBLE_DEVICES is used automagically remap the id since pynvml ignores this env var
+    if "CUDA_VISIBLE_DEVICES" in os.environ:
+        ids = list(map(int, os.environ.get("CUDA_VISIBLE_DEVICES", "").split(",")))
+        return ids[torch_gpu_id]  # remap
+    else:
+        return torch_gpu_id
+
+
+def initialize_correct_pynvml_device():
+    pynvml.nvmlInit()
+    torch_gpu_id = torch.cuda.current_device()
+    nvml_gpu_id = get_nvml_gpu_id(torch_gpu_id)
+    return pynvml.nvmlDeviceGetHandleByIndex(nvml_gpu_id)
