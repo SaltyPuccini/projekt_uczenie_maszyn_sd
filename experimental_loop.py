d4a1cb7823b23557ac8b5ecc1bf6f22c10ce8689
Kajetan Wojciechowski <63729377+SaltyPuccini@users.noreply.github.com>
2024-06-22 07:04:01 +0200

    Update experimental_loop.py
    Adjust experimental loop

--- experimental_loop.py
+++ experimental_loop.py
@@ -12,18 +12,14 @@ from utils.experiment_utils import WARM_UP_ITERATIONS, EXPERIMENT_ITERATIONS, mo
     SAVE_LOCATION_PREFIX, inference_steps, WARM_UP_STEPS, GUIDANCE_SCALE, WARM_UP_PROMPT
 from utils.logger import logger
 
-# Initialize nvml gpu measurement
 handle = initialize_correct_pynvml_device()
 
-# For each prompt and model, run the benchmarking loop
 for model_info in models_dict.items():
     logger.info("Running benchmark for model %s ", model_info[0])
-    # Initialize text2img pipeline for selected model
-    pipe = AutoPipelineForText2Image.from_pretrained(model_info[1], torch_dtype=torch.float16,
-                                                     variant="fp16")
+    
+    pipe = AutoPipelineForText2Image.from_pretrained(model_info[1], torch_dtype=torch.float16, variant="fp16")
     pipe.to('cuda')
 
-    # Warm up the model to stabilize performance
     logger.info("Warming up model")
     for _ in range(WARM_UP_ITERATIONS):
         gc.collect()
@@ -38,31 +34,26 @@ for model_info in models_dict.items():
         for inference_step in inference_steps:
             logger.info("Inferece step %d", inference_step)
 
-            # Initialize metrics list and start time
             metrics = []
             start_time = time.time()
 
-            # Run the benchmarking loop
             for i in range(EXPERIMENT_ITERATIONS):
                 logger.info("Running iteration %d", i)
-                # Clear GPU cache
+                
                 clear_gpu_cache()
 
-                # Run inference
                 start_infer_time = time.time()
                 image = \
                     pipe(prompt=PROMPT_PREFIX + prompt_info[1], num_inference_steps=inference_step,
                          guidance_scale=GUIDANCE_SCALE).images[0]
-
-                # Measure time and GPU metrics
                 end_infer_time = time.time()
+                
                 iteration_metrics = get_gpu_metrics(handle)
                 cpu_metrics = get_cpu_metrics()
                 iteration_metrics.update(cpu_metrics)
                 iteration_metrics['inference_time'] = end_infer_time - start_infer_time
                 metrics.append(iteration_metrics)
 
-                # Save image
                 image_dir = Path(SAVE_LOCATION_PREFIX,
                                  f'resulting_images/{model_info[0]}/{prompt_info[0]}/step_{inference_step}')
                 image_dir.mkdir(parents=True, exist_ok=True)
@@ -74,7 +65,6 @@ for model_info in models_dict.items():
             logger.info("Benchmarking loop complete")
             print(f"Total benchmarking time: {end_time - start_time:.2f} seconds")
 
-            # Convert metrics to DataFrame for analysis
             df_metrics = pd.DataFrame(metrics)
             csv_path = Path(SAVE_LOCATION_PREFIX, 'partial_csvs'
                             , f'{model_info[0]}_{prompt_info[0]}_step-{inference_step}.csv')
